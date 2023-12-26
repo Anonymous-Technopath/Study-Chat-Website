f600090babcf93bf07075535b7490e3e71229f35
Nimish Shandilya <93417822+Anonymous-Technopath@users.noreply.github.com>
2023-12-26 11:10:19 +0530

    changed urls.py and added string  variable to room routing , added the variable input along with request in views.py , now in order to go to a room we require to specify another value to take us to

--- base/views.py
+++ base/views.py
@@ -12,6 +12,6 @@ rooms =[
 def home(request):
     return render(request,'base/home.html',{'rooms':rooms})
 
-def room(request):
+def room(request,pk):
     return render(request,'base/room.html')
 
