4e92fa13029458f4291df605666afcacbb80c140
Nimish Shandilya <93417822+Anonymous-Technopath@users.noreply.github.com>
2023-12-26 21:43:12 +0530

    created delete.html for any kind of deletion, added deleteRoom function in views.py to delete a room, added route to delete-room in urls.py

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.home , name='home'),
     path('room/<str:pk>/',views.room, name='room'),
     path('create-room/',view=views.createRoom, name='create-room'),
-    path('update-room/<str:pk>/',view=views.updateRoom,name='update-room')
+    path('update-room/<str:pk>/',view=views.updateRoom,name='update-room'),
+    path('delete-room/<str:pk>/',view=views.deleteRoom,name='delete-room')
 ]
