0e0bff0478628176fd643a15cf545cc1bd832817
Nimish Shandilya <93417822+Anonymous-Technopath@users.noreply.github.com>
2024-01-04 12:58:59 +0530

    added getRoom to fetech single room data element, also created test.html to test api service and encountered cours problem so we are now installing cors headers package to deal with it

--- base/api/views.py
+++ base/api/views.py
@@ -19,3 +19,10 @@ def getRooms(request):
     rooms =Room.objects.all()
     serializer = RoomSerializer(rooms,many=True) # many means are there gonna be multiple objects we need to serialize is there going to be one
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getRoom(request,pk):
+    room =Room.objects.get(id=pk)
+    serializer = RoomSerializer(room,many=False) 
+    return Response(serializer.data)
