f51e82f4ad67ba8bca22f4331e8df25a11c8878e
Nimish Shandilya <93417822+Anonymous-Technopath@users.noreply.github.com>
2024-01-04 17:38:50 +0530

    Added avatar to custom user model,added media_url and media_root to settings, and configured the paths in urls.py

--- ChatWebsite/urls.py
+++ ChatWebsite/urls.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +11,5 @@ urlpatterns = [
     path('api/',include('base.api.urls'))
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)  # tells to set url to media url and get files from media root
